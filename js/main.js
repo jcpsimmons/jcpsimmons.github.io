945dff5c6e37ea10cc2f4aaad772c2b6f769310a
simsies <jcpsimmons@gmail.com>
2019-03-26 12:47:19 -0700

    turn off animations for repeat visitors, portfl. linked

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// find stylesheet by index - animate.css
+var animateStylesheet = document.styleSheets[3];
+
+// turn off main page animations for repeat visitors
+if ( firstImpression() ) {
+  console.log('New user - enjoy the animations');
+  animateStylesheet.disabled = false;
+} else {
+  console.log('Returning user - I\'ll disable the animations for you');
+  animateStylesheet.disabled = true;
+}
