555db8be8685475cc6f142802a4ef4965b71dcc7
simsies <jcpsimmons@gmail.com>
2019-04-13 15:13:29 -0700

    Linted the fuck up

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@
 var animateStylesheet = document.styleSheets[3];
 
 // turn off main page animations for repeat visitors
-if ( firstImpression() ) {
-  console.log('New user - enjoy the animations');
+if (firstImpression()) {
+  console.log("New user - enjoy the animations");
   animateStylesheet.disabled = false;
 } else {
-  console.log('Returning user - I\'ll disable the animations for you');
+  console.log("Returning user - I'll disable the animations for you");
   animateStylesheet.disabled = true;
 }
