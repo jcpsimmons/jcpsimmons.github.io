8c6565e8f0a4e76f4f5c7f7cc3cad6eb064b3cdb
simsies <jcpsimmons@gmail.com>
2019-04-28 16:09:19 -0700

    maps on maps on maps

--- client/src/components/portfolio/PortfolioBody.js
+++ client/src/components/portfolio/PortfolioBody.js
@@ -3,64 +3,141 @@ import React, { Component } from "react";
 const portfolioItems = [
   {
     title: "Abstruse",
-    image: "",
+    image: "img/project-photos/abstruse.png",
     info:
       "Website to document large-scale multimedia work for virtual reality and interactive electronics. The documentation website and VR beta application are linked below. I designed the VR application and the documentation website.",
-    techTags: "",
+    techTags: [
+      "Unity",
+      "C#",
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "jQuery",
+      "MaxMSP",
+      "Ableton Live",
+      "MIDI",
+      "OSC"
+    ],
     liveLink: "",
     gitHubLink: ""
   },
   {
     title: "Simple Texting UX Research",
-    image: "",
+    image: "img/project-photos/simple-texting.png",
     info:
       "Website to document large-scale multimedia work for virtual reality and interactive electronics. The documentation website and VR beta application are linked below. I designed the VR application and the documentation website.",
-    techTags: "",
+    techTags: [
+      "Unity",
+      "C#",
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "jQuery",
+      "MaxMSP",
+      "Ableton Live",
+      "MIDI",
+      "OSC"
+    ],
     liveLink: "",
     gitHubLink: ""
   },
   {
     title: "Greatreads Web App",
-    image: "",
+    image: "img/project-photos/mern-stack.png",
     info:
       "Website to document large-scale multimedia work for virtual reality and interactive electronics. The documentation website and VR beta application are linked below. I designed the VR application and the documentation website.",
-    techTags: "",
+    techTags: [
+      "Unity",
+      "C#",
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "jQuery",
+      "MaxMSP",
+      "Ableton Live",
+      "MIDI",
+      "OSC"
+    ],
     liveLink: "",
     gitHubLink: ""
   },
   {
     title: "Dream Machine",
-    image: "",
+    image: "img/project-photos/dream-machine.png",
     info:
       "Website to document large-scale multimedia work for virtual reality and interactive electronics. The documentation website and VR beta application are linked below. I designed the VR application and the documentation website.",
-    techTags: "",
+    techTags: [
+      "Unity",
+      "C#",
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "jQuery",
+      "MaxMSP",
+      "Ableton Live",
+      "MIDI",
+      "OSC"
+    ],
     liveLink: "",
     gitHubLink: ""
   },
   {
     title: "Dream Machine",
-    image: "",
+    image: "img/project-photos/abstruse.png",
     info:
       "Website to document large-scale multimedia work for virtual reality and interactive electronics. The documentation website and VR beta application are linked below. I designed the VR application and the documentation website.",
-    techTags: "",
+    techTags: [
+      "Unity",
+      "C#",
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "jQuery",
+      "MaxMSP",
+      "Ableton Live",
+      "MIDI",
+      "OSC"
+    ],
     liveLink: "",
     gitHubLink: ""
   },
   {
     title: "Dream Machine",
-    image: "",
+    image: "img/project-photos/abstruse.png",
     info:
       "Website to document large-scale multimedia work for virtual reality and interactive electronics. The documentation website and VR beta application are linked below. I designed the VR application and the documentation website.",
-    techTags: "",
+    techTags: [
+      "Unity",
+      "C#",
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "jQuery",
+      "MaxMSP",
+      "Ableton Live",
+      "MIDI",
+      "OSC"
+    ],
     liveLink: "",
     gitHubLink: ""
   },
   {
     title: "Dream Machine",
-    image: "",
+    image: "img/project-photos/abstruse.png",
     info:
       "Website to document large-scale multimedia work for virtual reality and interactive electronics. The documentation website and VR beta application are linked below. I designed the VR application and the documentation website.",
-    techTags: "",
+    techTags: [
+      "Unity",
+      "C#",
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "jQuery",
+      "MaxMSP",
+      "Ableton Live",
+      "MIDI",
+      "OSC"
+    ],
     liveLink: "",
     gitHubLink: ""
   }
@@ -69,14 +146,50 @@ const portfolioItems = [
 class PortfolioBody extends Component {
   render() {
     return (
-      <div>
-        {portfolioItems.map((item, index) => (
-          <div>
-            {index % 3 ? <p>noline</p> : <p>newline</p>}
-            <h1>{item.title}</h1>
-            <p>{index}</p>
-          </div>
-        ))}
+      <div className="container">
+        <div className="card-deck">
+          {portfolioItems.map((item, index) => (
+            <div key={index} className="col-lg-4">
+              <div className="card mb-4" id={index}>
+                <a href={item.liveLink}>
+                  <img
+                    class="card-img-top img-fluid"
+                    src={item.image}
+                    alt={item.title}
+                  />
+                </a>
+                <div class="card-body">
+                  <h4 class="card-title">{item.title}</h4>
+                  <p class="card-text">{item.info}</p>
+                  {item.techTags.map(tag => {
+                    return (
+                      <span className="badge badge-pill badge-primary mx-1">
+                        {tag}
+                      </span>
+                    );
+                  })}
+                </div>
+                <div class="card-footer">
+                  <a
+                    href="https://jcpsimmons.github.io/abstruse-landing-page/"
+                    class="btn btn-primary btn-sm btn-block"
+                    role="button"
+                  >
+                    View Live
+                  </a>
+                  <a
+                    href="https://github.com/jcpsimmons/spektra-public-prerelease"
+                    class="btn btn-secondary btn-sm btn-block github-button"
+                    role="button"
+                  >
+                    <i class="fab fa-github" />
+                    &nbsp;&nbsp;View on GitHub
+                  </a>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     );
   }
