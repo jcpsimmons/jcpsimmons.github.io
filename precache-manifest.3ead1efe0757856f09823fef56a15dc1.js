9e3aeb973b4b483c837c740c859f811fc253c631
simsies <jcpsimmons@gmail.com>
2019-05-01 14:02:55 -0700

    new build

---
+++ precache-manifest.3ead1efe0757856f09823fef56a15dc1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "768fe47ceb1b56380d444199011cc69c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4215e0206763f467f7aa",
+    "url": "./static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "dfba2df416908a387f3e",
+    "url": "./static/js/2.a0366da3.chunk.js"
+  },
+  {
+    "revision": "4215e0206763f467f7aa",
+    "url": "./static/js/main.2d8c08d2.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
